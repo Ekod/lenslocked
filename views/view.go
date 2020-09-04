937c9a88d0005ab35f75cd95a35ddb7d2fa9f9dd
Ekod <ekod87@yandex.ru>
2020-09-04 22:47:36 +0300

    Changed logic inside view.go

--- views/view.go
+++ views/view.go
@@ -1,22 +1,40 @@
 package views
 
-import "html/template"
+import (
+	"html/template"
+	"path/filepath"
+)
+
+var (
+	//LayoutDir is to used as path
+	LayoutDir string = "views/layouts/"
+	//TemplateExt is the extension for LayoutDir
+	TemplateExt string = ".gohtml"
+)
 
 //NewView func creates our templates
 func NewView(layout string, files ...string) *View {
-	files = append(files, "views/layouts/bootstrap.gohtml", "views/layouts/navbar.gohtml", "views/layouts/footer.gohtml")
+	files = append(files, layoutFiles()...)
 	t, err := template.ParseFiles(files...)
 	if err != nil {
 		panic(err)
 	}
 	return &View{
 		Template: t,
-		Layout: layout,
+		Layout:   layout,
 	}
 }
 
 //View type for NewView func
 type View struct {
 	Template *template.Template
-	Layout string
+	Layout   string
+}
+
+func layoutFiles() []string {
+	files, err := filepath.Glob(LayoutDir + "*" + TemplateExt)
+	if err != nil {
+		panic(err)
+	}
+	return files
 }
